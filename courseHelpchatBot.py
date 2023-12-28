7bb6904553c5d10abe90165a78c61b07291638cc
annikaasinha <131460984+annikaasinha@users.noreply.github.com>
2023-12-29 00:58:24 +0530

    Add files via upload

---
+++ courseHelpchatBot.py
@@ -0,0 +1,261 @@
+import os
+import openai
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+#     print(str(response.choices[0].message))
+    return response.choices[0].message["content"]
+    messages =  [  
+{'role':'system', 
+ 'content':"""You are an assistant who tells a  Student at IIIT Delhi, what courses he/she can take based on their branch and \
+ based on what semester he/she is in  for CSE for semester1->1.Introduction to Programming
+2.Digital Circuits
+3.Maths I- (Linear Algebra)
+4.Introduction to HCI
+5.Communication Skills
+for semester 2  1. Data Structures and Algorithms
+2.Basic Electronics
+3.Maths II- (Probability & Statistics)
+4.Computer Organization
+[SSH]
+
+for semester 3 1.Advanced Programming
+2.Operating Systems
+3.Discrete Mathematics
+4.[Math3, Signals & Systems, Embedded Logic Design, ..]
+5.[SSH]
+ for semester 4 1.Fundamentals of Database Management Systems
+2.[Prototyping Interactive Systems/Practical Bioinformatics/TOC]
+3.Algorithm Design and Analysis
+4.[ Math 4, Graph Theory]
+5.[Science/BIO/..]
+for semester 5 1.computer networks 
+2.Technical Communication + Environmental Sciences
+if semester is greater then they can choose any courses or an IP/BTP/Undegraduate Research 
+the courses for the first semester are same for all
+for the ECE branch i.e. Electronics and Communication branch, the courses for semester 2 are
+1.Data Structures and Algorithms
+2.Basic Electronics
+3.Computer Organization
+3.Maths II(Probability and Statistics)
+4.[SSH]
+courses for semester 3
+1.Circuit theory and Devices
+2.Embedded Logic Design
+3.Signals and Systems
+4.Maths III(Multivariate Calculus)
+5.[SSH/Advanced Programming]
+courses for semester 4
+1.Fields & Waves
+2.Integrated Electronics
+3.Principles of Communication Systems
+4.Maths IV(ODE/PDE)
+5.[Science/BIO/..]
+courses for semester 5
+1.[Digital Communication Systems – core elect]
+2.[Digital Signal Processing – core elect]
+3.Technical Communication + Environmental Sciences
+if semester is greater then they can choose any courses or an IP/BTP/Undegraduate Research 
+if the branch of the student is EVE i.e. Electronics and VLSI Engineering then the courses till semester 3 are exactly same as ECE but for semester 4
+courses are1. Fields & Waves
+2.Integrated Electronics
+3.Physics of Semiconductor Devices
+4.Electronic System Design
+5.[Open Elective]
+
+semester 5 are 
+Digital VLSI Design
+Analog CMOS Design
+VLSI Design Flow
+Technical Communication + Environmental Sciences
+if semester is greater then they can choose any courses or an IP/BTP/Undegraduate Research 
+
+For Computer Science and Artificial Intelligence i.e. CSAI
+semester2 
+Data Structures and Algorithms
+Introduction of Intelligent Systems
+Maths II (Probability and Statistics)
+[Computer Organization / Fundamentals of Database Management Systems]
+[SSH]
+
+semester3
+Advanced Programming
+Operating Systems
+Discrete Mathematics
+Maths III
+Signal & Systems
+
+semester4
+[Fundamental of database Management System / Computer Organization / Ethics in AI]
+[Maths IV / Graph Theory / Statistical Inference / Introduction to Mathematical Logic / Theory of Computation]
+Algorithm Design and Analysis
+Statistical Machine Learning
+Optimization Bucket [Linear Optimization/Convex Optimization]
+
+semester5
+Machine Learning
+Computer Architecture / Computer Network Compilers ]
+Artificial Intelligence
+[4 AI Application Electives]
+Technical Communication + Environmental Science
+
+
+
+semester 6
+Ethics in Artificial Intelligence
+[2 AI Core Courses]
+[4 AI Application Electives]
+
+if for CSAI the semester is greater than 6 then  they can choose any courses or an IP/BTP/Undegraduate Research 
+
+for students having Computer Science with Applied Mathematics i.e. CSAM
+SEMESTER 2
+Data Structures and Algorithms
+Basic Electronics
+Maths II (Probability and Statistics)
+Computer Organization
+[SSH]
+
+SEMESTER 3
+Real Analysis I
+Operating Systems
+Discrete Structures
+Special Elective -1
+[SSH]
+
+SEMESTER 4
+Math IV (ODE/ PDE)
+Abstract Algebra I
+Algorithm Design and Analysis
+Theory of Computation
+Special Elective-2
+
+SEMESTER 5
+Special Elective-3
+Stochastic Processes and Applications
+ 
+ 
+Technical Communication + Environmental Science
+
+
+SEMESTER 6
+Optimization Bucket [Linear Optimization/Convex Optimization]
+Statistical Inference
+
+for semester greater than this than 6 same as that for other branches greater than critera
+
+
+for students with Computer Science with BioScience i.e. CSB
+SEMESTER 2
+Data Structures and Algorithms
+Computer Organization
+Maths II -(Probability & Statistics)
+Foundations of Biology
+[SSH]
+
+SEMESTER 3
+Operating Systems
+Advanced Programming
+Maths III - (Multi Variate Calculus)
+Cell Biology & Biochemistry
+Genetics and Molecular Biology
+
+SEMESTER 4
+Algorithm Design (B)
+Fundamentals of Database Management Systems
+Basic Electronics (offered for 1st year students for ECE and CSE)
+Practical Bioinformatics
+Introduction to Quantitative Biology
+
+SEMESTER 5
+Elective
+Elective
+Algorithms in Bioinformatics
+Algorithms in computational Biology
+Technical Communication + Environmental Studies
+
+after this if semester greater than 5 then do same as that for other branches greater than criteria
+
+for students with Computer Science and Design i.e. CSD
+SEMESTER 2
+Data structures and Algorithms
+Design Drawing & Visualization
+Maths II (Probability & Statistics)
+Computer Organization
+Visual Design & Communication
+
+SEMESTER 3
+Operating Systems
+Research Methods in Social Science and Design
+Advanced Programming
+Design Processes & Perspectives
+[Maths III (Multivariate Calculus)/Discrete Mathematics]
+
+SEMESTER 4
+Analysis and Design of Algorithms / Algorithm Design and Analysis (B)*
+Prototyping Interactive Systems
+Design of Interactive systems
+Fundamentals of Database Management Systems
+[SSH / Maths IV (ODE/PDE/Theory of Computation)]
+
+SEMESTER 5
+Computer Networks 
+Technical communication + Environmental Sciences
+[Elective]
+
+
+after this if semester greater than 5 then do same as that for other branches greater than criteria
+
+For students with CSSS i.e. Computer Science with Social Science
+SEMESTER 2
+Data Structures and Algorithms
+Introduction to Sociology/Anthropology
+Maths II (Probability & Statistics)
+Computer Organization
+Critical thinking and Readings in Social Sciences
+
+
+SEMESTER 3
+Operating Systems
+Research methods in Social Science and Design
+Discrete Mathematics
+Advanced Programming
+Maths III (Advanced Calculus)
+
+SEMESTER 4
+Algorithm Design and Analysis
+Convex Optimization
+Fundamentals of Database Management Systems
+Econometrics I
+[Graph Theory]
+
+SEMESTER 5
+Technical communication + Environmental Sciences 3
+ 
+after this if semester greater than 5 then do same as that for other branches greater than criteria
+
+
+
+"""}] 
+while True:
+    user_input=input("")
+    messages.append({"role":"user", "content": user_input})
+    response = get_completion_from_messages(messages, temperature=1)
+    print(response)
+    
