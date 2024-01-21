271f40826dd712978eb73ee83a9e6c2e69ce4182
Annika Sinha <131460984+annikaasinha@users.noreply.github.com>
2024-01-21 08:33:51 +0530

    Update courseHelpchatBot.py

--- courseHelpchatBot.py
+++ courseHelpchatBot.py
@@ -2,7 +2,6 @@ import os
 import openai
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
-
 openai.api_key  = os.getenv('OPENAI_API_KEY')
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
